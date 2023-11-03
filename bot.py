5ee1cd5bd1fe8391df1fb02af26bc62b14653d12
nekita69 <kne.21@uni-dubna.ru>
2023-11-03 15:53:03 +0300

    fix bug

--- bot.py
+++ bot.py
@@ -6,34 +6,25 @@ from index import print_columns
 
 S_KEY = '6509438376:AAGSd-5pyyVl2PTbCOhlDn7KW-R6j2Dhwh0'
 
-# Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
-# Объект бота
 bot = Bot(token=S_KEY)
-# Диспетчер
 dp = Dispatcher()
 
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
-
-'''
-@dp.message(Command("stat"))
-async def cmd_stat(message: types.Message):
-'''
+    await message.answer("Hello! \n This bot represents the state of the pg_stat_activity database - /stat_db ")
 
 
-@dp.message(Command("col"))
+@dp.message(Command("stat_db"))
 async def cmd_col(message: types.Message):
     all = print_columns()
     for row in all:
         await message.answer(row)
 
-
-# Запуск процесса поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
--- index.py
+++ index.py
@@ -6,8 +6,10 @@ DB_NAME = 'testdb' #Имя БД
 USER = 'postgres' #Пользователь который работает с БД (в СУБД)
 DB_PASS = '123451515' #Пароль от пользователя
 
-connect1 = psycopg2.connect(host=HOST_DB, port=PORT_DB, dbname = DB_NAME , user= USER, password= DB_PASS)
+#dbname = DB_NAME
+connect1 = psycopg2.connect(host=HOST_DB, port=PORT_DB, user= USER, password= DB_PASS)
 curs1 = connect1.cursor()
+
 
 def print_columns():
     curs1.execute(f"SELECT column_name FROM information_schema.columns WHERE table_name = 'pg_stat_activity'")
@@ -21,3 +23,12 @@ def print_columns():
             temp += all_col[i][0] + " = " + str(row[i]) + "\n"   
         rez.append(temp)
     return rez
+
+'''
+def all_data():
+    curs1.execute("SELECT datname FROM pg_database")
+    rez = ""
+    for i in curs1.fetchall():
+        rez += i[0] + "\n"
+    return rez
+'''
