9403d45abefe2216204eec9c2df722dd817537c8
Илья <106013347+Bataevk@users.noreply.github.com>
2023-11-04 19:05:47 +0300

    fix

--- project/main.py
+++ project/main.py
@@ -7,6 +7,9 @@ from Services.config_DB import init_config
 from Services.config_DB import add_user
 from Services.config_DB import get_user
 from Models.user_models import User
+from Services.sys_status import start_thread, stop_thread, create_image
+from aiogram.types import FSInputFile
+from time import sleep
 
 S_KEY = '6509438376:AAGSd-5pyyVl2PTbCOhlDn7KW-R6j2Dhwh0'
 
@@ -17,14 +20,38 @@ dp = Dispatcher()
 
 # Хэндлер на команду /start
 @dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    
-    await message.answer("Hello! \n This bot represents the state of the pg_stat_activity database - /stat_db ", reply_markup = markup)
+async def start(message: types.Message):
+   await message.answer("Введите /help чтобы увидеть список доступных команд.", reply_markup = markup)
 
 
 @dp.message(Command("help"))
 async def help(message: types.Message):
-    await message.answer("/stat_db - вывести всю статистику \n/help - помощь", reply_markup=markup)
+    await message.answer("/help - помощь\n/system - посмотреть информацию о состоянии системы\nlisten - подписаться на сообщения о состоянии (доделать!)", reply_markup=markup)
+
+@dp.message(Command("system"))
+async def system(message: types.Message):
+    create_image()
+    system = FSInputFile('system.png')
+    await bot.send_photo(message.from_user.id, system)
+
+
+
+
+listening = False
+listeners = set()
+
+@dp.message(Command("listen"))
+async def listen(message: types.Message):
+    listeners.add(message.from_user.id)
+    print(listeners)
+
+    # тут надо проверить как дела и отправит ответ
+    # С помощью этого кусочка можно подписаться на бота 
+    # когда что-то слоаматется таким образом всех оповестить
+
+    # if что-то сломалось, то:
+    for _id in listeners:
+        await bot.send_message(_id, "Privet")
 
 
 # @dp.message(Command("stat_db"))
@@ -39,16 +66,19 @@ async def main():
     bob1 = User(name ="sss1", password = "lol",email =None,phonenumber = None, telegrm_id = None)
     add_user(bob)
     add_user(bob1)
-    print(get_user("sss1","lol").user_connections)
-   # await dp.start_polling(bot)
+    #print(get_user("sss1").user_connections)
+    await dp.start_polling(bot)
 
 
 kb = [
-        [types.KeyboardButton(text="/stat_db")]
+        [types.KeyboardButton(text="/help")]
+        [types.KeyboardButton(text="/system")]
+        [types.KeyboardButton(text="/listen")]
     ]
 markup = types.ReplyKeyboardMarkup(keyboard=kb)
 
 
 
 if __name__ == "__main__":
+    start_thread()
     asyncio.run(main())
