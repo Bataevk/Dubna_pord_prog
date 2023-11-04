c5d0e4a12706f6332dafea25d53c3428b3f10948
Victor <67371337+Gveideon@users.noreply.github.com>
2023-11-04 16:03:46 +0300

    small fix

--- project/Services/config_DB.py
+++ project/Services/config_DB.py
@@ -1,16 +1,22 @@
 from sqlalchemy import create_engine
-from project.Models.user_models import User
-from project.Models.user_models import Base
+from Models.user_models import User
+from Models.user_models import Base
 from sqlalchemy.orm import Session
 
 def init_config():
     global engine
     engine = create_engine("sqlite:///config.db")
     Base.metadata.create_all(bind=engine)
-def get_user(login,password):
+def get_user(login):
     with Session(autoflush=False, bind=engine) as db:
-        db.query(User).filter(User.name == login and User.password == password)
+        return db.query(User).filter(User.name == login).first()
 def add_user(user:User):
    with Session(autoflush=False, bind=engine) as db:
-       db.add(user)
-       db.commit()     
+       if get_user(user.login) is None:
+        db.add(user)
+        db.commit()     
+def remove_user(user:User):
+   with Session(autoflush = False, bind=engine) as db:
+       if get_user(user.login) is not None:
+        db.add(user)
+        db.commit() 
