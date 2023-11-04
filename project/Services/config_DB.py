c91ef9d46facfab49d863757d810bf9066919a84
Илья <106013347+Bataevk@users.noreply.github.com>
2023-11-04 15:48:19 +0300

    Add_Auth_pass

--- project/Services/config_DB.py
+++ project/Services/config_DB.py
@@ -7,10 +7,20 @@ def init_config():
     global engine
     engine = create_engine("sqlite:///config.db")
     Base.metadata.create_all(bind=engine)
-def get_user(login,password):
+
+
+def get_user(login):
     with Session(autoflush=False, bind=engine) as db:
-        db.query(User).filter(User.name == login and User.password == password)
+        return db.query(User).filter(User.name == login)
+
+
 def add_user(user:User):
-   with Session(autoflush=False, bind=engine) as db:
-       db.add(user)
-       db.commit()     
+    with Session(autoflush=False, bind=engine) as db:
+        try:
+            if get_user(user.name) is not None: 
+                return False
+            db.add(user)
+            db.commit()     
+            return True
+        except:
+            return False
